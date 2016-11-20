4cb39249bd9bf0124c8c3dcd62743dcc00285ead
meg0man <11gab12345@gmail.com>
2016-11-20 11:43:42 +0300

    beta 1.3

--- Graphs/GenericGraph/src/Edge.java
+++ Graphs/GenericGraph/src/Edge.java
@@ -1,50 +1,35 @@
 public class Edge<EdgeType> {
     private Vertex from, to;
     private EdgeType weight;
-    private Float cost;
-    private Float time;
 
-    public Edge(Vertex origin, Vertex destination, EdgeType weight, Float cost, Float time) {
-        this.from = origin;
-        this.to = destination;
-        this.weight = weight;
-        this.cost = cost;
-        this.time = time;
-    }
     public Edge(Vertex origin, Vertex destination, EdgeType weight) {
         this.from = origin;
         this.to = destination;
         this.weight = weight;
     }
 
+    public Vertex getOrigin() {
+        return from;
+    }
 
-    public Vertex getOrigin() { return from; }
     public Vertex getDest() {
         return to;
     }
+
     public EdgeType getWeight() {
         return weight;
-    }
-    public Float getCost() {
-        return cost;
-    }
-    public Float getTime() {
-        return time;
     }
 
     public void setOrigin(Vertex from) {
         this.from = from;
     }
+
     public void setDest(Vertex to) {
         this.to = to;
     }
+
     public void setWeight(EdgeType weight) {
         this.weight = weight;
     }
-    public void setCost(Float cost) {
-        this.cost = cost;
-    }
-    public void setTime(Float time) {
-        this.time = time;
-    }
 }
+
--- Graphs/GenericGraph/src/Vertex.java
+++ Graphs/GenericGraph/src/Vertex.java
@@ -1,7 +1,6 @@
 public class Vertex{
     private String name;
     private int id;
-    //List<Vertex> neighbours;
 
     public Vertex(String s, int id){
         this.name = s;
@@ -13,19 +12,11 @@ public class Vertex{
 
     @Override
     public boolean equals(Object o) {
-        /*if( o instanceof String)
-            return o.equals(name);
-        else*/
             return (o instanceof Vertex) && (((Vertex) o).getName().equals(name));
-
     }
     @Override
     public int hashCode() { return name.hashCode(); }
     @Override
     public String toString(){ return name; }
-
-    /*public void addNeighbour(Vertex v){
-        this.neighbours.add(v);
-    }*/
 }
 
